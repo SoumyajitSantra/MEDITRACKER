3d78fce18909dcc13a0c7edd1b0f265ae052d141
Soumyajit Santra <soumyajitsantra001@gmail.com>
2025-09-07 19:46:13 +0530

    Added navabr , foota rhome all mainstucture

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import { useState } from 'react'
-import Home from './components/Home'
+import { Routes, Route } from "react-router-dom";
+import Footer from './components/Footer'
 import './App.css'
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import AddStock from "./components/AddStock";
+import Sell from './components/sell';
+import Inventory from './components/Inventory';
+import AutoOrder from "./components/AutoOrder";
+
+
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Home/>
-    </>
-  )
+    
+    <div className="flex-grow pt-20 pb-20">
+      
+      <NavBar/>
+      <div className="flex-grow">
+        <Routes>
+          {/* Quick Links */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/add-stock" element={<AddStock />} />
+          <Route path="/sell" element={<Sell/>} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/auto-order" element={<AutoOrder />} />
+
+
+          {/* Default route */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+      
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Sell.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sell = () => {
+  return (
+    <div>S</div>
+  )
+}
+
+export default Sell;
